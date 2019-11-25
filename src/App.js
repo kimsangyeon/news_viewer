b2ee56d9fbe78c1b6ae5114fbf332dc984bfb7a9
Kim Sangyeon <dho113@naver.com>
2019-11-25 23:33:36 +0900

    newsapi API key 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try{
+      const response = await axios.get(
+        'https:/newsapi.org/v2/top-headlines?country=kr&apiKey=10b44c4e39ef4ac6af855e15169870de'
+      );
+      setData(response.data);
+    } catch(e) {
+      console.log(e);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
     </div>
   );
 }
