46effd7acf1b15a79bf0339875331e2fa70e0be6
Kim Sangyeon <dho113@naver.com>
2019-11-26 23:02:02 +0900

    newsapi data 연동

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try{
-      const response = await axios.get(
-        'https:/newsapi.org/v2/top-headlines?country=kr&apiKey=10b44c4e39ef4ac6af855e15169870de'
-      );
-      setData(response.data);
-    } catch(e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
-}
+  return <NewsList />;
+};
 
 export default App;
